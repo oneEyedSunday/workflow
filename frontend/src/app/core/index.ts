1bdf4f461a8dce46ae09889c5e01c62703c55f2e
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-07-20 21:07:44 +0100

    Auth Layout & Login template

--- frontend/src/app/core/index.ts
+++ frontend/src/app/core/index.ts
@@ -3,13 +3,16 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LandingComponent } from './landing/landing.component';
+import { AuthLayoutComponent } from './auth-layout.component';
 
 export const layoutCmps: any[] = [
     LayoutComponent,
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    LandingComponent
+    LandingComponent,
+    AuthLayoutComponent
 ];
 
 export * from './landing/landing.component';
+export * from './auth-layout.component';
