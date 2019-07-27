bd84724a70aac4ef12f8355d2530d36b7eb52bf5
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-07-27 20:56:11 +0100

    Hook up process & document services to API

--- frontend/src/app/documents/services/documents.service.ts
+++ frontend/src/app/documents/services/documents.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { Document } from '@shared/interfaces';
-
+import { AppConfig } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HandleError, HttpErrorHandler } from '@shared/http-error-handler.service';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class DocumentService {
+    private readonly handleError: HandleError;
+    private readonly url = AppConfig.API_URL;
+    constructor(
+        private http: HttpClient,
+        httpErrorHandler: HttpErrorHandler
+    ) {
+        this.handleError = httpErrorHandler.createHandleError('Documents');
+    }
+
     fetchDocuments(): Observable<Document[]> {
-        return of([]);
+        return this.http.get(this.url + '/process/document')
+            .pipe(
+                map(response => response),
+                catchError(this.handleError('Fetch Documnets', null))
+            );
     }
 
     getDocumentById(identifier: number | string): Observable<Document> {
-        return of (new Document());
+        return this.http.get(this.url + '/process/document/' + identifier).pipe(
+            map(response => response),
+            catchError(this.handleError('Fetch Document', null))
+        );
     }
 }
--- frontend/src/app/projects/services/projects.service.ts
+++ frontend/src/app/projects/services/projects.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Process } from '@shared/interfaces';
 import { Observable, of } from 'rxjs';
+import { AppConfig } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HandleError, HttpErrorHandler } from '@shared/http-error-handler.service';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
-
-  constructor() { }
+  private readonly handleError: HandleError;
+  private readonly url = AppConfig.API_URL;
+  constructor(
+      private http: HttpClient,
+      httpErrorHandler: HttpErrorHandler
+  ) {
+      this.handleError = httpErrorHandler.createHandleError('Documents');
+  }
 
   fetchProcesses(): Observable<Process[]> {
-    return of([
-      new Process(), new Process(), new Process()
-    ]);
+    return this.http.get(this.url + '/process').pipe(
+      map(response => response),
+      catchError(this.handleError('Fetch Projects', null))
+    );
   }
 
   getProcessById(id: number | string): Observable<Process> {
-    return of(new Process());
+    return this.http.get(this.url + '/process/' + id).pipe(
+      map(response => response),
+      catchError(this.handleError('Fetch Project', null))
+    );
   }
 }
