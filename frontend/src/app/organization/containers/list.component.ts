32f2fc6bdfd97be95047cfa9fff3281e4fcd757b
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-08-06 23:34:35 +0100

    Update Group Description Card Structure

--- frontend/src/app/organization/containers/list.component.ts
+++ frontend/src/app/organization/containers/list.component.ts
@@ -10,18 +10,19 @@ export class GroupListComponent implements OnInit {
 
   ngOnInit(): void {
     this.groups = [
+      // tslint:disable-next-line: max-line-length
+      new Group({description: 'fter dragging Android File Transfer to the Applications folder, hold down the control key while clicking in the window and select Eject “Android File Transfer” from the menu. You can also delete the DMG file by moving it to the Trash.', group_name: 'janananana'}),
+      new Group({description: 'anannana', group_name: 'janananana'}),
+      new Group({description: 'This is an evangelical group', group_name: 'janananana'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
+      new Group({description: 'No swearing in this xtain subreddit', group_name: 'Wholesome Memes'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
       new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
+      new Group({description: 'We are all retards', group_name: 'WSB'}),
       new Group()
     ];
   }
