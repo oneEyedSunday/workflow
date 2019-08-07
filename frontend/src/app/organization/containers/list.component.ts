3716faf884e981c2bf7caa231235b71b5e8e95f7
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-08-07 14:16:34 +0100

    Hook up List to API

--- frontend/src/app/organization/containers/list.component.ts
+++ frontend/src/app/organization/containers/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Group, IGroup } from '@shared/interfaces';
+import { GroupsService } from '../services';
+import { finalize } from 'rxjs/operators';
 
 @Component({
     selector: 'app-group-list',
@@ -7,23 +9,26 @@ import { Group, IGroup } from '@shared/interfaces';
 })
 export class GroupListComponent implements OnInit {
   groups: Group[];
+  groupsAndIcon: Group[];
+  loading: boolean;
+
+  constructor(private _groupsSvc: GroupsService) { }
 
   ngOnInit(): void {
-    this.groups = [
-      // tslint:disable-next-line: max-line-length
-      new Group({description: 'fter dragging Android File Transfer to the Applications folder, hold down the control key while clicking in the window and select Eject “Android File Transfer” from the menu. You can also delete the DMG file by moving it to the Trash.', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'This is an evangelical group', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'No swearing in this xtain subreddit', group_name: 'Wholesome Memes'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'anannana', group_name: 'janananana'}),
-      new Group({description: 'We are all retards', group_name: 'WSB'}),
-      new Group()
-    ];
+    this.getGroups();
+  }
+
+  getGroups(): void {
+    this.loading = true;
+    this._groupsSvc.fetchGroups()
+      .pipe(
+        finalize(() => this.loading = false)
+      ).subscribe(
+        groups => {
+          this.groups = groups;
+          this.groupsAndIcon = [...groups, null as Group];
+        },
+        () => this.loading = false
+      );
   }
 }
