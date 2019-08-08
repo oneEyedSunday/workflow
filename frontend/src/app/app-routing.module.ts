e51439e06f43f2263058a5e5732e1c6f304377a8
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-08-08 11:34:24 +0100

    Guard routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './core/layout/layout.component';
 import { LandingComponent, AuthLayoutComponent } from './core';
+import { AuthGuard } from '@shared/guards';
 
 const routes: Routes = [
   {
@@ -21,6 +22,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'projects',
